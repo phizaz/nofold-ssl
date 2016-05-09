48877f93de53b669426c64434316b453eca5e171
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-05-09 19:59:44 +0700

    adding rfam seed databases

---
+++ src/create_seed_file_by_family.py
@@ -0,0 +1,36 @@
+import os, sys
+from os.path import isfile, join
+from Bio import SeqIO
+from multiprocessing.dummy import Pool as ThreadPool
+
+root = 'Rfam-seed/fasta'
+
+files = [f for f in os.listdir(root) if isfile(join(root, f)) and f != '.DS_Store']
+
+def worker(file):
+    family = file.split('.')[0]
+    path = join(root, file)
+    print(path)
+
+    alt_records = []
+    with open(path) as file:
+        records = SeqIO.parse(file, 'fasta')
+        for record in records:
+            # alter the record by adding family in front
+            record.id = family + '.' + record.id
+            record.name = family + '.' + record.name
+            alt_records.append(record)
+
+    output_db = join('Rfam-seed', 'db', family, family + '.db')
+    # create dir along the path if not exists
+    db_path = os.path.dirname(output_db)
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
+
+    with open(output_db, 'w') as handle:
+        SeqIO.write(alt_records, handle, 'fasta')
+
+POOL_SIZE = len(files)
+pool = ThreadPool(processes=POOL_SIZE)
+
+pool.map(worker, files)
---
+++ src/score_and_normalize_rfam_seed.py
@@ -0,0 +1,21 @@
+import os, sys
+from utils_analysis import *
+
+path = '../Rfam-seed/db'
+
+families = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f)) and len(list(map(lambda x: x != '.DS_Store', os.listdir(os.path.join(path, f))))) == 1]
+
+for family in families:
+    print(family)
+
+    command = 'python %s %s --cpus=%d' % (
+        'score_and_normalize.py',
+        os.path.join(path, family, family + '.db'),
+        4
+    )
+    (output, res, error) = run_command(command)
+
+    if error:
+        print ">>Error detected. Exiting."
+        print(output)
+        sys.exit()
