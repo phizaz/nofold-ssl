fab14b9d7bf04a0f3291c8247b9e47e2e1684662
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-05-19 15:22:48 +0700

    add scripts

--- compile_seed.sh
+++ compile_seed.sh
@@ -1,1 +1,6 @@
-# 
+# compile seed for cmscores
+cd src
+python score_and_normalize_rfam_seed.py --cpus=8 --begin=RF01900
+
+# or
+nohup python score_and_normalize_rfam_seed.py --cpus=8 --begin=RF01900 &
