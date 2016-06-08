b173edd804b238ed6c55fa3976934eb32b9c39e5
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-06-09 00:46:34 +0700

    analyse Rfam database and a script to create query based on its distribution

---
+++ create_query.py
@@ -0,0 +1,97 @@
+from __future__ import division
+from os.path import join, exists
+import utils
+import gzip
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import SingleLetterAlphabet
+from Bio.SeqRecord import SeqRecord
+from collections import Counter
+from random import randint, sample
+import numpy as np
+
+# record = (fam, name, sequence)
+
+def fam_of(record):
+    return record[0]
+
+def name_of(record):
+    return record[1]
+
+def seq_of(record):
+    return record[2]
+
+def decorate_name(fam, name):
+    return fam + '_' + name.split('/')[0]
+
+def get_char_distribution(string):
+    counter = Counter()
+    for char in string:
+        counter[char] += 1
+    for key in counter.keys():
+        counter[key] /= len(string)
+    return counter
+
+def bioseq_of((fam, name, sequence)):
+    return SeqRecord(Seq(sequence, SingleLetterAlphabet),
+                     id=name, name=name, description='')
+
+def weighted_random(cnt, possibles, weights):
+    return ''.join(np.random.choice(possibles, cnt, p=weights))
+
+def generate_background(records, generate_size):
+    pass
+
+def embed_sequence(sequence, embed_size):
+    distribution = get_char_distribution(sequence)
+    embed_front = weighted_random(embed_size, distribution.keys(), distribution.values())
+    embed_rear = weighted_random(embed_size, distribution.keys(), distribution.values())
+    return embed_front + sequence + embed_rear
+
+def embed(records, (embed_min_length, embed_max_length)):
+    results = []
+    for fam, name, sequence in records:
+        embed_size = randint(embed_min_length, embed_max_length)
+        results.append((fam, name + '_emb', embed_sequence(sequence, embed_size)))
+    return results
+
+def save_records(file, records):
+    seq_records = map(bioseq_of, records)
+    with open(file, 'w') as handle:
+        SeqIO.write(seq_records, handle, 'fasta')
+
+query = 'fam40_typedistributed_embedded'
+
+rfam_path = '/Volumes/Konpat/Rfam 12.1'
+selected_fams = ['RF01848', 'RF02044', 'RF00727', 'RF02402', 'RF01607', 'RF01687', 'RF01685', 'RF02375', 'RF02514', 'RF01660', 'RF01225', 'RF01323', 'RF01689', 'RF00052', 'RF01786', 'RF02089', 'RF01482', 'RF01909', 'RF01929', 'RF01833', 'RF01501', 'RF01500', 'RF01505', 'RF01504', 'RF01506', 'RF01293', 'RF00381', 'RF00027', 'RF02045', 'RF00222', 'RF00104', 'RF00103', 'RF01930', 'RF01931', 'RF01932', 'RF01933', 'RF01934', 'RF01496', 'RF01499', 'RF01498']
+
+total_seq_cnt = 1000
+seq_per_fam = int(total_seq_cnt / len(selected_fams))
+
+all_embed_records = []
+for fam in selected_fams:
+    print('fam:', fam)
+    names_taken = Counter()
+    with gzip.open(join(rfam_path, fam + '.fa.gz')) as handle:
+        raw_records = list(SeqIO.parse(handle, 'fasta'))
+        print('size:', len(raw_records))
+
+        sampled_records = sample(raw_records, seq_per_fam)
+        print(sampled_records)
+
+        records = []
+        for record in sampled_records:
+            decorated_name = decorate_name(fam, record.name)
+            names_taken[decorated_name] += 1
+
+            if names_taken[decorated_name] > 1:
+                decorated_name += ':' + str(names_taken[decorated_name])
+
+            records.append((fam, decorated_name, str(record.seq)))
+
+        embed_records = embed(records, (10, 50))
+        all_embed_records += embed_records
+
+print('saving..')
+save_records(join('quries', query, query + '.db'), all_embed_records)
+print('done..')
---
+++ playground/analyse_rfam_description.py
@@ -0,0 +1,117 @@
+from __future__ import division
+import csv
+from collections import Counter
+import numpy as np
+from operator import itemgetter
+
+def create_map(strings):
+    all_strings = set(strings)
+    all_int = [i for i in range(len(all_strings))]
+    str_to_int = dict(zip(all_strings, all_int))
+    int_to_str = dict(zip(all_int, all_strings))
+
+    def map_to_int(string):
+        return str_to_int[string]
+
+    def map_to_str(int):
+        return int_to_str[int]
+
+    return map_to_int, map_to_str
+
+def bit_transform(types):
+    have = np.zeros(len(possible_types))
+    for t in types:
+        s = to_int(t)
+        have[s] = 1
+    return have
+
+def score(bits, distribution):
+    s = 0
+    for a, expect in zip(bits, distribution):
+        s += min(a, expect)
+    return s
+
+def discretize(bits):
+    return np.array(map(float, map(int, bits)))
+
+
+file = 'rfam_description.csv'
+
+select_family_cnt = 40
+select_sequence_cnt = 1000
+select_per_family = select_sequence_cnt / select_family_cnt
+
+families = []
+type_freq = Counter()
+with open(file, 'r') as handle:
+    reader = csv.reader(handle, delimiter=',')
+    for name, fam, type, seed, full, description in reader:
+        types = filter(lambda x: x, map(lambda x: x.strip(), type.strip().split(';')))
+        for t in types:
+            type_freq[t] += 1
+
+        families.append((name, fam, types, int(seed), int(full), description))
+
+possible_types = type_freq.keys()
+print('possible types:', possible_types)
+to_int, to_type = create_map(possible_types)
+
+distrubition = np.zeros(len(possible_types))
+weights = np.ones(len(possible_types))
+for type, freq in sorted(type_freq.items(), key=lambda x: -x[1]):
+    weighted_freq = freq / len(families)
+    distrubition[to_int(type)] = weighted_freq
+    print type, ':', freq
+
+print('type cnt:', len(type_freq))
+print('distribution:', distrubition)
+
+bits = map(bit_transform, map(itemgetter(2), families))
+fam_bits = sorted(zip(families, bits), key=lambda x: -sum(x[1]))
+max_bit_cnt = int(sum(fam_bits[0][1]))
+print('max_bit_cnt:', max_bit_cnt)
+
+have = np.zeros(len(possible_types))
+
+# more lnc RNA
+# weights[to_int('lncRNA')] = 3
+
+expected_distribution = distrubition * weights * 40 # arbitrary number
+
+print('expected_distribution:')
+print(discretize(expected_distribution))
+
+last_score = score(have, expected_distribution)
+picked = set()
+for threshold in range(max_bit_cnt, 0, -1):
+    # don't over pick
+    if len(picked) >= select_family_cnt:
+        break
+
+    for (name, fam, types, seed, full, description), bits in fam_bits:
+        # family doesn't have enough sequences
+        if full < select_per_family:
+            continue
+
+        # don't pick many times
+        if fam in picked:
+            continue
+
+        tmp = have + bits
+        new_score = score(tmp, expected_distribution)
+        if new_score - last_score >= threshold:
+            picked.add(fam)
+            have = tmp
+            last_score = new_score
+
+        if len(picked) >= select_family_cnt:
+            break
+
+print('picked:', len(picked))
+print(have)
+for i, each in enumerate(have):
+    if each > 0:
+        print to_type(i), ':', int(each)
+print(picked)
+
+
