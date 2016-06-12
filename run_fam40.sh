fad7c0025a08b5b154f73446a853cd132bd5eec3
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-06-12 11:23:56 +0700

    add fam40 (no embedded) queries

---
+++ run_fam40.sh
@@ -0,0 +1,4 @@
+python score_and_normalize.py ../queries/fam40_typedistributed/fam40_typedistributed.db --cpus=8 && \
+python score_and_normalize.py ../queries/fam40_typedistributed_bg/fam40_typedistributed_bg.db --cpus=8 && \
+python score_and_normalize.py ../queries/fam40_typedistributed_embed/fam40_typedistributed_embed.db --cpus=8 && \
+python score_and_normalize.py ../queries/fam40_typedistributed_embed_bg/fam40_typedistributed_embed_bg.db --cpus=8
