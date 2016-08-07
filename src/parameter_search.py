e7a6170f51234ea67283e31e5b3e1e1e14840082
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-08-07 10:24:19 +0700

    update parameter search range

--- src/parameter_search.py
+++ src/parameter_search.py
@@ -107,7 +107,7 @@ search_space = {
     'alg': ['labelSpreading', 'labelPropagation'],
     # 'alg': ['labelSpreading'],
     'kernel': ['rbf'],
-    'gamma': [0.5],
+    'gamma': np.linspace(0.5, 1.0, 6),
     'alpha': np.linspace(0.4, 1.0, 7),
     # 'alpha': [1.0],
     'c': np.linspace(1.0, 1.5, 6),
