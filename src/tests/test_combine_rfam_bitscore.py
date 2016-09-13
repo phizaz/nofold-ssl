225d65da7a95907411d0dd44272896b70e1bb5ee
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-09-13 21:06:29 +0700

    adding RF01050

--- src/tests/test_combine_rfam_bitscore.py
+++ src/tests/test_combine_rfam_bitscore.py
@@ -11,7 +11,7 @@ class CombineRfamBitscore(unittest.TestCase):
         for each in points:
             self.assertEqual(len(each), 1973)
 
-        inc_names, inc_points, inc_header = run('test_rna', False, 1, 2, True)
+        inc_names, inc_points, inc_header = run('test_rna', False, 0, 1, True)
 
         self.assertEqual(len(inc_names), len(inc_points))
         self.assertEqual(len(inc_header), 1973)
@@ -23,18 +23,18 @@ class CombineRfamBitscore(unittest.TestCase):
 
 
     def test_real(self):
-        import utils
+        from src import utils
         from os.path import join
         output = utils.run.run_python_attach_output(join(utils.path.src_path(), 'combine_rfam_bitscore.py'),
                                                     query='test_rna',
-                                                    cripple=1,
-                                                    nn=2)
+                                                    cripple=0,
+                                                    nn=1)
         print(output)
 
         _names, _points, _header = utils.get.get_bitscores(
-            join(utils.path.results_path(), 'combined.test_rna.cripple1.bitscore'))
+            join(utils.path.results_path(), 'combined.test_rna.cripple0.bitscore'))
 
-        names, points, header = run('test_rna', False, 1, 2)
+        names, points, header = run('test_rna', False, 0, 1, True)
         self.assertEqual(len(header), 1973)
         for each in points:
             self.assertEqual(len(each), 1973)
