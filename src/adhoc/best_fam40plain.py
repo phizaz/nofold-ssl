f110964cf6fd245bb8de142ea89944112d517ea5
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-11-08 20:56:11 +0700

    update bestfam40

--- src/adhoc/best_fam40plain.py
+++ src/adhoc/best_fam40plain.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
     rows = open_result(join(utils.path.results_path(), file))
 
     # filter rows
-    # rows = get_rows_filtered(['cripple'], ['6'], rows) # 0.4 is overall not bad
+    rows = get_rows_filtered(['nn_seed', 'cripple', 'multilabel', 'merge', 'c'], ['19', '20', 'False', 'False', '1.3'],
+                             rows)  # 0.4 is overall not bad
 
     args, best_vals = analyse(rows, dist_l2)
+    print(best_vals)
 
     target_files = [
         ('itself', file),
