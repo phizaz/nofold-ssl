c701ad82dc47ee79b2ffaca75f1463e0e952c8b6
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-11-12 11:45:31 +0700

    testing memory leak from combine rfam

---
+++ src/adhoc/test_memory_combine_rfam.py
@@ -0,0 +1,18 @@
+'''
+To see if the memory leakage happens in combine_rfam_bitscore or not ?
+'''
+if __name__ == '__main__':
+    from src import combine_rfam_bitscore
+
+    query = 'fam40_typedistributed_embed+bg_weak'
+
+    from src.utils.helpers import sizeof
+    for i in range(10):
+        print('round {}'.format(i + 1))
+        # leakage comes from here
+        names, points, header = combine_rfam_bitscore.run(query, False, 6, 7, False, None)
+        print('size of names : {}'.format(sizeof.total_size(names)))
+        print('size of points : {}'.format(sizeof.total_size(points)))
+        print('size of header : {}'.format(sizeof.total_size(header)))
+
+    print('done!')
